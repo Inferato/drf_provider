51430c0212be794f779f5be446c1557e7c746b75
Inferato <infaustmager@gmail.com>
2024-04-12 18:43:30 +0300

    noqa

--- example/settings.py
+++ example/settings.py
@@ -78,12 +78,8 @@ WSGI_APPLICATION = 'example.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Example',
-        'USER': 'superuser',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
